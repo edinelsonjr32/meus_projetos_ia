c68d459c903dea295291eb6ae6b2bfa3f45d3a7c
cod3cafe <edinelsonjr.ufopa@gmail.com>
2021-11-05 19:25:48 -0300

    Criaçao de perceptron com treinamento de taxa de aprendizagem

--- Redes_Neurais/perceptron_com_numpy.py
+++ Redes_Neurais/perceptron_com_numpy.py
@@ -1,3 +1,4 @@
+# importação da biblioteca numpy
 import numpy as np
 
 entradas = np.array([-1, 7, 5])
@@ -8,11 +9,17 @@ pesos = np.array([0.8, 0.1, 0])
 def soma(e, p):
     return e.dot(p)
     # dotProduto ou produto escalar
+
+
 s = soma(entradas, pesos)
 print(s)
+
+
 def stepFunction(soma):
     if soma >= 1:
         return 1
     return 0
+
+
 r = stepFunction(s)
 print(r)
